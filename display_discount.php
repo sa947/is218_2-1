327196806f37838052c027f80fef2949b69b7017
sa947 <sa947@njit.edu>
2017-06-18 15:51:50 -0400

    Added php statements 5

--- display_discount.php
+++ display_discount.php
@@ -5,36 +5,54 @@
 	        
 		    $discount = $list_price * $discount_percent * .01;
 		        $discount_price = $list_price - $discount;
-			    ?>
-			    <!DOCTYPE html>
-			    <html>
-			    <head>
-			    <title>Product Discount Calculator</title>
-			    <link rel="stylesheet" type="text/css"
-			    href="main.css">
-			    </head>
-			    <body>
-			    <main>
+			    
+			        $list_price_f = "$".number_format($list_price,
+				2);
+				    $discount_percent_f =
+				    $discount_percent."%";
+				        $discount_f =
+					"$".number_format($discount, 2);
+					    $discount_price_f =
+					    "$".number_format($discount_price,
+					    2);
+					        ?>
 
-			    <label>Product Description:</label>
-			    <span><?php echo $product_description;
-			    ?></span><br>
+						<!DOCTYPE html>
+						<html>
+						<head>
+						<title>Product Discount
+						Calculator</title>
+						<link rel="stylesheet"
+						type="text/css"
+						href="main.css">
+						</head>
+						<body>
+						<main>
 
-			    <label>List Price:</label>
-			    <span><?php echo $list_price; ?></span><br>
+						<label>Product
+						Description:</label>
+						<span><?php echo
+						$product_description;
+						?></span><br>
 
-			    <label>Standard Discount:</label>
-			    <span><?php echo $discount_percent; ?></span><br>
+						<label>List Price:</label>
+						<span><?php echo $list_price;
+						?></span><br>
 
+						<label>Standard
+						Discount:</label>
+						<span><?php echo
+						$discount_percent;
+						?></span><br>
 
-			    <label>Discount Amount:</label>
-			    <span><?php echo $discount; ?></span><br>
+						<label>Discount Amount:</label>
+						<span><?php echo $discount_f;
+						?></span><br>
 
-			    <label>Discount Price:</label>
-			    <span><?php echo $discount_price; ?></span><br>
-
-			    </main>
-			    </body>
-			    </html>
-
-
+						<label>Discount Price:</label>
+						<span><?php echo
+						$discount_price_f;
+						?></span><br>
+						</main>
+						</body>
+						</html>
