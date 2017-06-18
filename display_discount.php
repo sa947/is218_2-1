47641086990ba788e81fdc3d3e971c503e06a417
sa947 <sa947@njit.edu>
2017-06-18 15:47:10 -0400

    Added php statements 3

--- display_discount.php
+++ display_discount.php
@@ -1,27 +1,29 @@
-
 <?php
-    $product_description = $_POST['product_description'];
-        $list_price = $_POST['list_price'];
-	    $discount_percent = $_POST['discount_percent'];
-	    ?>
-	    <!DOCTYPE html>
-	    <html>
-	    <head>
-	    <title>Product Discount Calculator</title>
-	    <link rel="stylesheet" type="text/css" href="main.css">
-	    </head>
-	    <body>
-	    <main>
+    
+        $product_description = filter_input(INPUT_POST, 'product_description');
+	    $list_price = filter_input(INPUT_POST, 'list_price');
+	        $discount_percent = filter_input(INPUT_POST,
+		'discount_percent');
+		    
+		        ?>
+			<!DOCTYPE html>
+			<html>
+			<head>
+			<title>Product Discount Calculator</title>
+			<link rel="stylesheet" type="text/css" href="main.css">
+			</head>
+			<body>
+			<main>
 
-	    <label>Product Description:</label>
-	    <span><?php echo $product_description; ?></span><br>
+			<label>Product Description:</label>
+			<span><?php echo $product_description; ?></span><br>
 
-	    <label>List Price:</label>
-	    <span><?php echo $list_price; ?></span><br>
+			<label>List Price:</label>
+			<span><?php echo $list_price; ?></span><br>
 
-	    <label>Standard Discount:</label>
-	    <span><?php echo $discount_percent; ?></span><br>
+			<label>Standard Discount:</label>
+			<span><?php echo $discount_percent; ?></span><br>
 
-	    </main>
-	    </body>
-	    </html>
+			</main>
+			</body>
+			</html>
