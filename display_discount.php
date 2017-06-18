ca875cefa0b57edf2a7fcbefc06a141c2b528d36
sa947 <sa947@njit.edu>
2017-06-18 15:49:42 -0400

    Added php statements 4

--- display_discount.php
+++ display_discount.php
@@ -1,29 +1,40 @@
 <?php
-    
-        $product_description = filter_input(INPUT_POST, 'product_description');
-	    $list_price = filter_input(INPUT_POST, 'list_price');
-	        $discount_percent = filter_input(INPUT_POST,
-		'discount_percent');
-		    
-		        ?>
-			<!DOCTYPE html>
-			<html>
-			<head>
-			<title>Product Discount Calculator</title>
-			<link rel="stylesheet" type="text/css" href="main.css">
-			</head>
-			<body>
-			<main>
+    $product_description = filter_input(INPUT_POST, 'product_description');
+        $list_price = filter_input(INPUT_POST, 'list_price');
+	    $discount_percent = filter_input(INPUT_POST, 'discount_percent');
+	        
+		    $discount = $list_price * $discount_percent * .01;
+		        $discount_price = $list_price - $discount;
+			    ?>
+			    <!DOCTYPE html>
+			    <html>
+			    <head>
+			    <title>Product Discount Calculator</title>
+			    <link rel="stylesheet" type="text/css"
+			    href="main.css">
+			    </head>
+			    <body>
+			    <main>
 
-			<label>Product Description:</label>
-			<span><?php echo $product_description; ?></span><br>
+			    <label>Product Description:</label>
+			    <span><?php echo $product_description;
+			    ?></span><br>
 
-			<label>List Price:</label>
-			<span><?php echo $list_price; ?></span><br>
+			    <label>List Price:</label>
+			    <span><?php echo $list_price; ?></span><br>
 
-			<label>Standard Discount:</label>
-			<span><?php echo $discount_percent; ?></span><br>
+			    <label>Standard Discount:</label>
+			    <span><?php echo $discount_percent; ?></span><br>
 
-			</main>
-			</body>
-			</html>
+
+			    <label>Discount Amount:</label>
+			    <span><?php echo $discount; ?></span><br>
+
+			    <label>Discount Price:</label>
+			    <span><?php echo $discount_price; ?></span><br>
+
+			    </main>
+			    </body>
+			    </html>
+
+
